d0de85b48a933122582b6130f67fca2d556a4b2c
ming@zmeet <jiaozhu@abcpen.com>
2022-11-16 08:51:38 +0800

    update key

--- python/demo_tts_long.py
+++ python/demo_tts_long.py
@@ -21,15 +21,15 @@ async def main():
     parser = argparse.ArgumentParser(description="ASR Server offline audio file demo",
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-u', '--url', type=str, metavar='URL',
-                        help='server url', default='ai.abcpen.com')
+                        help='server url', default='tts.yitutech.com')
     parser.add_argument('-l', '--log_path', type=str, metavar='LOG',
                         help='log file path', default='asr_res.log')
 
     args = parser.parse_args()
     
     ## 下面的app_id 和api_key仅供测试使用，生产环境请向商务申请(手机：18605811078, 邮箱：jiaozhu@abcpen.com)
-    app_id = "test1"
-    api_key = "2258ACC4-199B-4DCB-B6F3-C2485C63E85A"
+    app_id = ""
+    api_key = ""
     if (len(app_id)<=0 or len(api_key)<=0):
         print("Please apply appid and appsecret, demo will exit now")
         sys.exit(1)
@@ -41,7 +41,7 @@ async def main():
         "appid": app_id,
         "signa": signa,
         "content": "满足用户高性价比需求的消费场景；通过流量集中分发，构成好品质加极致价格力的特卖场，满足极速版商城用户的购物需求，提高用户粘性，形成用户‘日销好货’的心智",
-        "audioType": 11
+        "audioType": 5
 
     }
     url = "http://{}/v2/tts/long".format(args.url)
