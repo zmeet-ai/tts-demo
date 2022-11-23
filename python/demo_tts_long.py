d040b54653dda13cc6e9f00f48a6450859ef9ee6
ming@zmeet <jiaozhu@abcpen.com>
2022-11-23 17:38:33 +0800

    yitu style

--- python/demo_tts_long.py
+++ python/demo_tts_long.py
@@ -21,13 +21,13 @@ async def main():
     parser = argparse.ArgumentParser(description="ASR Server offline audio file demo",
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-u', '--url', type=str, metavar='URL',
-                        help='server url', default='ai.abcpen.com')
+                        help='server url', default='tts.yitutech.com')
     parser.add_argument('-l', '--log_path', type=str, metavar='LOG',
                         help='log file path', default='asr_res.log')
 
     args = parser.parse_args()
     
-    ## 下面的app_id 和api_key仅供测试使用，生产环境请向商务申请(手机：18605811078, 邮箱：jiaozhu@abcpen.com)
+    ## 下面的app_id 和api_key仅供测试使用，生产环境请向商务申请
     app_id = ""
     api_key = ""
     if (len(app_id)<=0 or len(api_key)<=0):
@@ -45,10 +45,11 @@ async def main():
 
     }
     url = "http://{}/v2/tts/long".format(args.url)
+    ##!!! 按照 x-www-form-urlencoded 提交请求；java，go等代码请都按照 x-www-form-urlencoded 提交请求。
     response = requests.post(url, querys)
     print(response.text)
 
-    response_json = json.loads(response.text)
+    #response_json = json.loads(response.text)
 
 
 if __name__ == "__main__":
